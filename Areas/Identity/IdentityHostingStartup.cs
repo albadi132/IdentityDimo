c9e2e1842a9f5247b3a0d9227fd72e8c0d3d0c71
Salim AL-Badi <alkaisar967@gmail.com>
2021-12-23 12:29:47 +0400

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using IdentityDimo.Areas.Identity.Data;
+using IdentityDimo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityDimo.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityDimo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityDimoContext>(options =>
+                    options.UseMySql(
+                        context.Configuration.GetConnectionString("IdentityDimoContextConnection")));
+
+                services.AddDefaultIdentity<IdentityDimoUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<IdentityDimoContext>();
+            });
+        }
+    }
+}
---
+++ Controllers/RoleController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDimo.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        [Authorize(Policy = "rolecreation")]
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "rolecreation")]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
